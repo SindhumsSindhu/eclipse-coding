77bdd54174972b93c2da0210560d007732b5445e
SindhumsSindhu <138276765+SindhumsSindhu@users.noreply.github.com>
2023-11-02 16:13:48 +0530

    Add files via upload

---
+++ nonprimitive/src/Localvariable/lv.java
@@ -0,0 +1,21 @@
+package Localvariable;
+
+public class lv {
+	
+	public void StudentAge()
+	{
+	
+	// Local variable 
+	int age = 0;
+	age = age + 30;
+	System.out.println("Student age is : " + age);
+	}
+	public static void main(String[] args) {
+	//Local Variable
+	lv temp = new lv();
+	temp.StudentAge();
+	}
+	}
+
+
+
---
+++ nonprimitive/src/nonprimitive/np.java
@@ -0,0 +1,13 @@
+package nonprimitive;
+
+public class np {
+
+	public static void main(String[] args) {
+		String s="hello";
+		System.out.println("String "+s);
+		String s1=new String("World");
+		System.out.println("String1"+s1);
+
+	}
+
+}
